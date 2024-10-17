717dbe627d605a2c7156648874f94924f37477da
chaosi-zju <chaosi@zju.edu.cn>
2024-10-17 19:45:02 +0800

    init

--- karmada-demo/spark/install.sh
+++ karmada-demo/spark/install.sh
@@ -7,9 +7,9 @@ docker pull apache/spark:v3.1.3
 kind load docker-image apache/spark:v3.1.3 --name karmada-host
 kind load docker-image apache/spark:v3.1.3 --name member1
 
-helm repo add spark-operator https://kubeflow.github.io/spark-operator
-helm repo update
-helm install spark-operator spark-operator/spark-operator \
+#helm repo add spark-operator https://kubeflow.github.io/spark-operator
+#helm repo update
+helm install spark-operator ./spark-operator/ \
     --namespace karmada-system \
     --create-namespace
 
